ab591655369ed7bb76e2c613295a2f19ce99ef8d
Oliver <oliverkreutz@hotmail.com>
2020-11-15 02:01:55 +0100

    Reuploaded Task 3

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    private bool menuActive = false;
+    private bool needUpdate = true;
+
+    private void Update()
+    {
+        UserInput();
+
+        MenuHandling();
+    }
+
+    private void MenuHandling()
+    {
+        if (!needUpdate) return;
+        if (menuActive)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        pauseMenu.SetActive(menuActive);
+        needUpdate = false;
+    }
+
+    private void UserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuActive = !menuActive;
+            needUpdate = true;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Return()
+    {
+        menuActive = false;
+        needUpdate = true;
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    private int scoreCount = 0;
+    [SerializeField ]private TextMeshProUGUI scoreCountText;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        scoreCountText.text = scoreCount.ToString();
+    }
+
+    public void IncreaseScore()
+    {
+        scoreCount++;
+        UpdateText();
+        Debug.Log(scoreCount);
+    }
+
+    internal void DecreaseScore()
+    {
+        scoreCount--;
+        UpdateText();
+        Debug.Log(scoreCount);
+    }
+}
